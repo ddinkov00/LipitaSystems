cb6827ded691157023ab498660259a35068d7813
DDinkov00 <65896735+ddinkov00@users.noreply.github.com>
2021-01-13 21:01:13 +0200

    Db update

--- Data/LipitaSystems.Data/Migrations/20210113190036_ContavtMessageTableUpdated.Designer.cs
+++ Data/LipitaSystems.Data/Migrations/20210113190036_ContavtMessageTableUpdated.Designer.cs
@@ -4,14 +4,16 @@ using LipitaSystems.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace LipitaSystems.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210113190036_ContavtMessageTableUpdated")]
+    partial class ContavtMessageTableUpdated
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/LipitaSystems.Data/Migrations/20210113190036_ContavtMessageTableUpdated.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LipitaSystems.Data.Migrations
+{
+    public partial class ContavtMessageTableUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Email",
+                table: "ContactMessages",
+                newName: "Contact");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Contact",
+                table: "ContactMessages",
+                newName: "Email");
+        }
+    }
+}
