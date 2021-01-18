a9ee3725acd2c6568a98eb389dc048aefff10513
DDinkov00 <65896735+ddinkov00@users.noreply.github.com>
2021-01-18 13:37:46 +0200

    Identity scaffolded

---
+++ Web/LipitaSystems.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using LipitaSystems.Data;
+using LipitaSystems.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LipitaSystems.Web.Areas.Identity.IdentityHostingStartup))]
+namespace LipitaSystems.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
